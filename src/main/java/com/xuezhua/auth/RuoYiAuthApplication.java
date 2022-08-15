d85f96215e14e9c5df2f4651200ed555c42b4d74
小猪 <810780738@qq.com>
2022-08-15 17:16:40 +0800

    first commit

---
+++ src/main/java/com/xuezhua/auth/RuoYiAuthApplication.java
@@ -0,0 +1,22 @@
+package com.xuezhua.auth;
+
+import com.xuezhua.security.annotation.EnableRyFeignClients;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 认证授权中心
+ * 
+ * @author ruoyi
+ */
+@EnableRyFeignClients
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class RuoYiAuthApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RuoYiAuthApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  认证授权中心启动成功   ლ(´ڡ`ლ)ﾞ");
+    }
+}
---
+++ src/main/java/com/xuezhua/auth/service/SysRecordLogService.java
@@ -0,0 +1,49 @@
+package com.xuezhua.auth.service;
+
+import com.xuezhua.core.constant.Constants;
+import com.xuezhua.core.constant.SecurityConstants;
+import com.xuezhua.core.utils.ServletUtils;
+import com.xuezhua.core.utils.StringUtils;
+import com.xuezhua.core.utils.ip.IpUtils;
+import com.xuezhua.system.api.RemoteLogService;
+import com.xuezhua.system.api.domain.SysLogininfor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 记录日志方法
+ * 
+ * @author ruoyi
+ */
+@Component
+public class SysRecordLogService
+{
+    @Autowired
+    private RemoteLogService remoteLogService;
+
+    /**
+     * 记录登录信息
+     * 
+     * @param username 用户名
+     * @param status 状态
+     * @param message 消息内容
+     * @return
+     */
+    public void recordLogininfor(String username, String status, String message)
+    {
+        SysLogininfor logininfor = new SysLogininfor();
+        logininfor.setUserName(username);
+        logininfor.setIpaddr(IpUtils.getIpAddr(ServletUtils.getRequest()));
+        logininfor.setMsg(message);
+        // 日志状态
+        if (StringUtils.equalsAny(status, Constants.LOGIN_SUCCESS, Constants.LOGOUT, Constants.REGISTER))
+        {
+            logininfor.setStatus(Constants.LOGIN_SUCCESS_STATUS);
+        }
+        else if (Constants.LOGIN_FAIL.equals(status))
+        {
+            logininfor.setStatus(Constants.LOGIN_FAIL_STATUS);
+        }
+        remoteLogService.saveLogininfor(logininfor, SecurityConstants.INNER);
+    }
+}
